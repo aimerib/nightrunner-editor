ea86a6c35260a10967a3fb71f00213698121a701
Aimeri Baddouh <aimeri@baddouh.me>
2022-01-09 19:34:33 -0500

    add initial settings page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Input, ListContainer } from '../../components';
+import { useFocus } from '../../utils';
 
 export default function Settings() {
-  return <></>;
+  const [name, set_name] = useState('');
+  const [intro, set_intro] = useState('');
+  const [inputRef, setInputFocus] = useFocus();
+  return (
+    <div className="flex flex-col w-full h-full gap-5 bg-nr-main text-green-nr">
+      <Input
+        label="Game Name:"
+        name="description"
+        autoFocus
+        value={name}
+        onChange={(e) => set_name(e.target.value)}
+        innerRef={inputRef}
+      />
+      <Input
+        label="Game Introduction:"
+        name="text"
+        multiline
+        value={intro}
+        onChange={(e) => {
+          set_intro(e.target.value);
+        }}
+      />
+      <Button type="button" className="self-center">
+        Save Game
+      </Button>
+    </div>
+  );
 }

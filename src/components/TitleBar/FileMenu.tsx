5f0ade7050c36c840b7b6197299b3ece804dfe7b
Aimeri Baddouh <aimeri@baddouh.me>
2022-01-09 19:35:54 -0500

    add other submenus and warning modal add missing save and load game options. Still missing functionality. Adds a warning modal for new game to warn users of potential of data loss in unsaved games.

--- src/components/TitleBar/FileMenu.tsx
+++ src/components/TitleBar/FileMenu.tsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 // import { open } from '@tauri-apps/api/dialog';
 // import { notification } from '@tauri-apps/api';
 import { store } from '../../store';
+import { WarningModal } from '../../components';
 
 interface Props {
   active: boolean;
@@ -38,23 +39,52 @@ interface Props {
 
 export const FileMenu = ({ active, innerRef, setActive }: Props) => {
   const { new_game } = useContext(store);
+  const [show_warning, set_show_warning] = useState(false);
 
   return (
-    <div
-      ref={innerRef}
-      className={`${
-        active ? 'visible' : 'hidden'
-      } z-50 absolute bg-gray-900 text-green-nr w-1/12 menu_area menu_background`}
-    >
-      <a
-        className="menu_item"
-        onClick={() => {
-          setActive(false);
-          new_game();
-        }}
+    <>
+      <div
+        ref={innerRef}
+        className={`${
+          active ? 'visible' : 'hidden'
+        } z-50 absolute bg-gray-900 text-green-nr w-1/12 menu_area menu_background`}
       >
-        New Game
-      </a>
-    </div>
+        <a
+          className="menu_item"
+          onClick={() => {
+            setActive(false);
+            set_show_warning(true);
+          }}
+        >
+          New Game
+        </a>
+        <a
+          className="menu_item"
+          onClick={() => {
+            setActive(false);
+            new_game();
+          }}
+        >
+          Save Game
+        </a>
+        <a
+          className="menu_item"
+          onClick={() => {
+            setActive(false);
+            new_game();
+          }}
+        >
+          Load Game
+        </a>
+      </div>
+      <WarningModal
+        show={show_warning}
+        handle_close={() => set_show_warning(false)}
+        title="Are you sure?"
+        callback={new_game}
+      >
+        <p>This will discard any unsaved progress.</p>
+      </WarningModal>
+    </>
   );
 };
